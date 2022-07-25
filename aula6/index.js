785e3b41982ecc3ca43cc0cf0a71ddf94159cb59
inaldo.monteiro <inaldo.monteiro@adagri.ce.gov.br>
2022-07-25 15:48:10 -0300

    Estudo Javascript

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+  //um comentário
+  console.log('Olá mundo 1');
+  console.log('Este trecho será exibido no console do navegador 1');
+  console.log('Olá mundo 2');
+  console.log('Este trecho será exibido no console do navegador 2');
+  console.log('Olá mundo 3');
+  console.log('Este trecho será exibido no console do navegador 3');
--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log('Olá mundo'); //Aqui tem outro comentário
  * Comentários de várias linhas Javascript
  * Autho:Inaldo Monteiro
  */
+
+
+    //um comentário
+    console.log('Olá mundo');
+    console.log('Este trecho será exibido no console do navegador');
